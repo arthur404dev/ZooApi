c8471290bfd771bb8e3894bfbcef9a6dee4d2b2a
Arthur Andrade <arthur.diegoo@hotmail.com>
2020-02-23 16:43:27 -0300

    Add Sample Data and First Route
    As this is only a boilerplate project for any application, i'm trying to be as consise as possible, that's why i'll be using only sample data and not an external DB.
    
    Renamed the project for easier referral.

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+
 )
 
 // Animal Structs
@@ -24,10 +26,15 @@ type Species struct {
 	Endangered    string `json:"endangered"`
 }
 
+// Global var of Animal Slice (This is only for this small app, as we're not using an external DB)
+// TODO: Implement DB
+var animals []Animal
+
 // Functions
 // Fetch All Animals
 func getAnimals(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(animals)
 }
 
 // Fetch Single Animal
@@ -53,6 +60,11 @@ func deleteAnimal(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Start the router for the main endpoint - Going to use Mux for this project
 	router := mux.NewRouter()
+	// *Sample Data (This is only due to the fact we're not using a DB for this project)
+	// TODO: Implement DB
+	animals = append(animals, Animal{ID: "1", Name: "Hector", Diet: "Fruits", Species: &Species{Name: "Chimp", Family: "Mammal", Domestication: "Domestic", Endangered: "No"}})
+	animals = append(animals, Animal{ID: "2", Name: "Lauritz", Diet: "Fruits", Species: &Species{Name: "Giraffe", Family: "Mammal", Domestication: "Wild", Endangered: "Yes"}})
+	animals = append(animals, Animal{ID: "3", Name: "Karl", Diet: "Meat", Species: &Species{Name: "Dog", Family: "Canine", Domestication: "Domestic", Endangered: "No"}})
 
 	// Create route endpoints + handlers - Create the CRUD handlers here (Using animals for this example, change to your usage)
 	router.HandleFunc("/api/animals", getAnimals).Methods("GET")
